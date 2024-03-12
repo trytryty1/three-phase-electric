61d3df585ae79737d65013a290419f8699f7a81e
trytryty1 <58746198+trytryty1@users.noreply.github.com>
2024-03-12 11:23:08 -0700

    correct link on hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -24,17 +24,17 @@ export default function Hero() {
             </p>
             <div className="mt-10 flex items-center gap-x-6">
               <a
-                href="#"
+                href="tel:931-345-4664"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Contact Us
               </a>
-              <a
+              {/* <a
                 href="#"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
                 Learn more <span aria-hidden="true">→</span>
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
