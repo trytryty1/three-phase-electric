9de218703125fcae783243c05f7d63f99d9359fc
trytryty1 <58746198+trytryty1@users.noreply.github.com>
2024-03-12 14:49:42 -0700

    updated logo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -10,14 +10,14 @@ function classNames(...classes) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-secondary-900 shadow">
+    <Disclosure as="nav" className="bg-gray-800 shadow">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-white-400 hover:bg-gray-100 hover:text-white-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -36,28 +36,28 @@ export default function Navbar() {
                   />
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                  {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                  {/* Current: "border-indigo-500 text-white-900", Default: "border-transparent text-white-500 hover:border-gray-300 hover:text-white-700" */}
                   <a
                     href="#"
-                    className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
+                    className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-white-900"
                   >
                     Home
                   </a>
                   {/* <a
                     href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white-500 hover:border-gray-300 hover:text-white-700"
                   >
                     Team
                   </a>
                   <a
                     href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white-500 hover:border-gray-300 hover:text-white-700"
                   >
                     Projects
                   </a>
                   <a
                     href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white-500 hover:border-gray-300 hover:text-white-700"
                   >
                     Calendar
                   </a> */}
@@ -76,7 +76,7 @@ export default function Navbar() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 pb-4 pt-2">
-              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-white-500 hover:bg-gray-50 hover:border-gray-300 hover:text-white-700" */}
               <Disclosure.Button
                 as="a"
                 href="#"
@@ -87,7 +87,7 @@ export default function Navbar() {
               <Disclosure.Button
                 as="a"
                 href="tel:931-345-4664"
-                className="border-l-4 flex flex-row border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                className="border-l-4 flex flex-row border-transparent py-2 pl-3 pr-4 text-base font-medium text-white-500 hover:border-gray-300 hover:bg-gray-50 hover:text-white-700"
               >
                 Call Now
                 <PhoneArrowUpRightIcon className="h-5 w-5 ml-2" />
@@ -95,14 +95,14 @@ export default function Navbar() {
               {/* <Disclosure.Button
                 as="a"
                 href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white-500 hover:border-gray-300 hover:bg-gray-50 hover:text-white-700"
               >
                 Projects
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
                 href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white-500 hover:border-gray-300 hover:bg-gray-50 hover:text-white-700"
               >
                 Calendar
               </Disclosure.Button> */}
