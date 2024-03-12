525edf372f0aade3eaaa12c23f012ab071305c2e
trytryty1 <58746198+trytryty1@users.noreply.github.com>
2024-03-12 11:22:09 -0700

    updated metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,27 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Three Phase Electric",
   description: "Safe, Reliable, Professional Electrical Contractor in Lorem ipsum",
+  image: "/logo.png",
+  robots: "follow, index",
+  locale: "en_US",
+  site_name: "Three Phase Electric",
+  type: "website",
+  url: "https://threephaseelectricllc.com",
+  phone: "931-345-4664",
+  email: "threephaseelectric.tn@gmail.com",
+  address: "Clarksville, TN 37040",
+  region: "Tennessee",
+  country: "United States",
+
+  // Open Graph
+  og: {
+    title: "Three Phase Electric",
+    description: "Safe, Reliable, Professional Electrical Contractor in Clarksville, TN",
+    image: "/logo.png",
+    url: "https://threephaseelectricllc.com",
+    type: "website",
+    site_name: "Three Phase Electric",
+  },
 };
 
 export default function RootLayout({ children }) {
