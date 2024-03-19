6bcc438af057dccc649b0c882af1bcd2680d50f5
trytryty1 <58746198+trytryty1@users.noreply.github.com>
2024-03-18 17:57:54 -0700

    added solar image

---
+++ src/app/about-us/page.jsx
@@ -0,0 +1,20 @@
+export default function AboutUs() {
+  return (
+    <div className="relative bg-background">
+      <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
+        <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
+          <main className="mx-auto max-w-2xl lg:mx-0">
+            <article>
+              <h2 className="mt-24 text-xl font-bold tracking-tight text-primary-900 sm:mt-10 sm:text-3xl">
+                Ryland Barron
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-primary-600">
+                
+              </p>
+            </article>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
